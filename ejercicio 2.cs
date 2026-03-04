fd28e59ffdef27020186d6e6c9d8778cb4467c15
javierlopez19 <humberthojavier@gmail.com>
2026-03-03 19:15:28 -0600

    Create ejercicio 2.cs

---
+++ ejercicio 2.cs
@@ -0,0 +1,12 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("El Banco Industrial ha decidido aumentar el límite de crédito en sus tarjetas, indique que tipo de cliente es(1,2,3 u otro (4)): ");
+
+
+
+
+    }
+}
