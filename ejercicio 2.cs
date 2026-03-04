a4493313bab9686edcb19b530c62ff0687e7d5e4
javierlopez19 <humberthojavier@gmail.com>
2026-03-03 19:36:01 -0600

    Update ejercicio 2.cs

--- ejercicio 2.cs
+++ ejercicio 2.cs
@@ -3,19 +3,24 @@ class Program
 {
     static void Main()
     {
-        Console.WriteLine("El Banco Industrial ha decidido aumentar el límite de crédito en sus tarjetas, indique que tipo de cliente es (1,2,3 u otro 4): ");
+        Console.WriteLine("El Banco Industrial ha decidido aumentar el límite de crédito en sus tarjetas");
+        Console.WriteLine("Ingrese su límite actual");
+        double limite = double.Parse(Console.ReadLine());
+
+        Console.WriteLine("Ingrese que tipo de cliente es, 1, 2, 3 u otro 4: ");
         int cliente = int.Parse(Console.ReadLine());
+        double porcentaje = 0; 
 
         switch (cliente)
         {
             case 1:
-                Console.WriteLine("Cliente Tipo 1 aumentará un 25%"); break;
+                porcentaje = 0.25; break;
             case 2:
-                Console.WriteLine("Cliente Tipo 2 aumentará un 35%"); break;
+                porcentaje = 0.35; break;
             case 3:
-                Console.WriteLine("Cliente Tipo 3 aumentará un 40%"); break;
+                porcentaje = 0.40; break;
             default:
-                Console.WriteLine("Ustes es otro tipo de cliente entonces, aumentará un 50%"); break;
+                porcentaje = 0.50; break;
         }
 
 
